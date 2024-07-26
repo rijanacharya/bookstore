8dc24a80ca7e73ff42c948f77d672e92fa9a8798
Rijan <rijanacharya100@gmail.com>
2024-07-26 13:37:28 -0400

    adding book java

---
+++ src/main/java/com/maar/bookstore/books/Book.java
@@ -0,0 +1,68 @@
+package com.maar.bookstore.books;
+
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+public class Book {
+
+    private Long id;
+    private String name;
+    private String author;
+
+   // Book(){}
+
+    public Book(Long id,String name, String author)
+    {
+        this.id = id;
+        this.author = author;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id) && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/maar/bookstore/books/BookService.java
@@ -0,0 +1,15 @@
+package com.maar.bookstore.books;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+
+    public List<Book> getStudents() {
+        return List.of(
+                new Book(1L,"Rich dad poor dad","arthur nery")
+        );
+    }
+}
